a17eb1e8302a9d36d73ce7c7da9a12256caa660c
niklas-hjelm <niklas.hjelm@iths.se>
2024-04-04 13:35:26 +0200

    update

--- Domain/Entities/Forum/ForumThread.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Domain.Entities.Forum;
-
-public class ForumThread : IEntity<string>
-{
-    public virtual string Id { get; set; }
-    public string Title { get; set; }
-    public IList<ForumPost> Posts { get; set; }
-}
--- Infrastructure/DataAccess/Forum/Documents/ForumThreadDocument.cs
+++ Infrastructure/DataAccess/Forum/Documents/ForumThreadDocument.cs
@@ -1,12 +1,16 @@
-﻿using Domain.Entities.Forum;
+﻿using Domain.Entities;
+using Domain.Entities.Forum;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 
 namespace Infrastructure.DataAccess.Forum.Documents;
 
-public class ForumThreadDocument : ForumThread
+public class ForumThreadDocument : IEntity<string>
 {
     [BsonId]
     [BsonRepresentation(BsonType.ObjectId)]
-    public override string Id { get; set; }
+    public string Id { get; set; }
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public List<ForumPost> Posts { get; set; }
 }
